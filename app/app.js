3773d48eb37f86c2914c54fb7defc6a6129de582
Kyle Bradshaw <kyle@somedirection.com>
2015-04-28 05:03:12 -0400

    wonky animations added

--- app/app.js
+++ app/app.js
@@ -7,6 +7,19 @@
             'app.home',
             'app.countries'
         ])
+        .run(['$rootScope', '$location', '$timeout', function($rootScope, $location, $timeout) {
+            $rootScope.$on('$routeChangeError', function() {
+                $location.path("/error");
+            });
+            $rootScope.$on('$routeChangeStart', function() {
+                $rootScope.isLoading = true;
+            });
+            $rootScope.$on('$routeChangeSuccess', function() {
+              $timeout(function() {
+                $rootScope.isLoading = false;
+              }, 1000);
+            });
+        }])
         .config(function($locationProvider, $routeProvider) {
             // $locationProvider.hashPrefix('!');
             $routeProvider.otherwise({
